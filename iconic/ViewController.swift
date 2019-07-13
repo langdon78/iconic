3ccd50d9ad149a48f5c4710b5b33c2632bf1dda8
James Langdon <corporatelangdon@gmail.com>
2019-07-13 17:17:22 -0400

    Initial commit

---
+++ iconic/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  iconic
+//
+//  Created by James Langdon on 7/13/19.
+//  Copyright © 2019 corporatelangdon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
