edde656e2c8d2d79190790373c001d422d9d55bf
James Langdon <corporatelangdon@gmail.com>
2019-07-14 11:15:17 -0400

    Rename algorithm identifier

--- iconic/NounAPI/AuthClient/Crypto.swift
+++ iconic/NounAPI/AuthClient/Crypto.swift
@@ -13,9 +13,10 @@ import CommonCrypto
 /// using supported methods from
 /// CommonCrypto library (i.e. MD5, SHA1 etc.)
 class Crypto {
+    
     struct HashAlgorithm {
         private(set) var length: Int32
-        private(set) var key: UInt32
+        private(set) var ccIdentifier: UInt32
         private var lengthInt: Int {
             return Int(length)
         }
@@ -23,9 +24,9 @@ class Crypto {
             return [UInt8](repeating: 0, count: lengthInt)
         }
         
-        init(length: Int32, key: Int) {
+        init(length: Int32, ccIdentifier: Int) {
             self.length = length
-            self.key = CCHmacAlgorithm(key)
+            self.ccIdentifier = CCHmacAlgorithm(ccIdentifier)
         }
     }
     
@@ -39,12 +40,12 @@ class Crypto {
         
         var algorithm: HashAlgorithm {
             switch self {
-            case .md5: return HashAlgorithm(length: CC_MD5_DIGEST_LENGTH, key: kCCHmacAlgMD5)
-            case .sha1: return HashAlgorithm(length: CC_SHA1_DIGEST_LENGTH, key: kCCHmacAlgSHA1)
-            case .sha224: return HashAlgorithm(length: CC_SHA224_DIGEST_LENGTH, key: kCCHmacAlgSHA224)
-            case .sha256: return HashAlgorithm(length: CC_SHA256_DIGEST_LENGTH, key: kCCHmacAlgSHA256)
-            case .sha384: return HashAlgorithm(length: CC_SHA384_DIGEST_LENGTH, key: kCCHmacAlgSHA384)
-            case .sha512: return HashAlgorithm(length: CC_SHA512_DIGEST_LENGTH, key: kCCHmacAlgSHA512)
+            case .md5: return HashAlgorithm(length: CC_MD5_DIGEST_LENGTH, ccIdentifier: kCCHmacAlgMD5)
+            case .sha1: return HashAlgorithm(length: CC_SHA1_DIGEST_LENGTH, ccIdentifier: kCCHmacAlgSHA1)
+            case .sha224: return HashAlgorithm(length: CC_SHA224_DIGEST_LENGTH, ccIdentifier: kCCHmacAlgSHA224)
+            case .sha256: return HashAlgorithm(length: CC_SHA256_DIGEST_LENGTH, ccIdentifier: kCCHmacAlgSHA256)
+            case .sha384: return HashAlgorithm(length: CC_SHA384_DIGEST_LENGTH, ccIdentifier: kCCHmacAlgSHA384)
+            case .sha512: return HashAlgorithm(length: CC_SHA512_DIGEST_LENGTH, ccIdentifier: kCCHmacAlgSHA512)
             }
         }
     }
@@ -67,11 +68,21 @@ class Crypto {
             using hash: HashAlgorithmType,
             with key:   String
         ) -> Result<String, EncryptionError>  {
+        
+        // Exit early if parameters malformed
         guard !message.isEmpty else { return .failure(.emptyMessage) }
         guard !key.isEmpty else { return .failure(.emptyKey) }
         
+        // C-style function call
         var result = hash.algorithm.allocated
-        CCHmac(hash.algorithm.key, key, key.count, message, message.count, &result)
+        CCHmac(hash.algorithm.ccIdentifier,
+                key,
+                key.count,
+                message,
+                message.count,
+                &result)
+        
         return .success(Data(result).base64EncodedString())
     }
+    
 }
