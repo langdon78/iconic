54e233ae72dde00c80fa409d5c75c4fdc658b064
James Langdon <corporatelangdon@gmail.com>
2019-07-14 08:48:49 -0400

    Add NounAPIClient file

---
+++ iconic/NounAPI/NounAPIClient.swift
@@ -0,0 +1,13 @@
+//
+//  NounAPIClient.swift
+//  iconic
+//
+//  Created by James Langdon on 7/13/19.
+//  Copyright © 2019 corporatelangdon. All rights reserved.
+//
+
+import Foundation
+
+class NounAPIClient {
+    
+}
