fb1184bff1a2342d06acd712f5bd5f6f480a1b69
James Langdon <corporatelangdon@gmail.com>
2019-07-14 22:54:34 -0400

    Add collection view handling and icon display

--- iconic/ViewController.swift
+++ iconic/ViewController.swift
@@ -9,8 +9,54 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var collectionView: UICollectionView!
+    var icons: [Icon] = [] {
+        didSet {
+            DispatchQueue.main.async {
+                self.collectionView.reloadData()
+            }
+        }
+    }
+    var nounApiClient = NounAPIClient()
+    var page = 1
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        fetchRecentUpdates(for: page)
+    }
+    
+    func fetchRecentUpdates(for page: Int? = nil) {
+        nounApiClient.recentUploads(limit: 20, page: page) { result in
+            switch result {
+            case .success(let recent): self.icons.append(contentsOf: recent.recentUploads)
+            case .failure(let error): print(error)
+            }
+        }
+    }
+}
+
+// MARK: UICollectionView Data Source
+extension ViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return icons.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "iconCell", for: indexPath) as? IconCollectionViewCell
+        let icon = icons[indexPath.row]
+        cell?.configure(with: icon)
+        return cell ?? UICollectionViewCell()
+    }
+    
+}
+
+extension ViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+        if (indexPath.row == icons.endIndex - 1) {
+            page += 1
+            fetchRecentUpdates(for: page)
+        }
     }
 }
